e94558d51d83e0e9ff0f8b90fe5e0faa3fcceabb
Alan Douglas Aranda <alandouglasaranda@Alans-MacBook-Pro-2.local>
2023-04-09 10:24:28 +0200

    Add worker

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
-let audio1 = new Audio()
-audio1.src = 'modjo-lady.mp3'
+container.addEventListener('click', function () {
+  let audio1 = new Audio()
+  audio1.src = 'modjo-lady.mp3'
 
-const container = document.getElementById('container')
-const canvas = document.getElementById('canvas')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+  let canvas = document.getElementById('canvas').transferControlToOffscreen()
+  const worker = new Worker(new URL('./worker.js', import.meta.url))
 
-const ctx = canvas.getContext('2d')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  worker.postMessage({ canvas }, [canvas])
 
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-let audioSource = null
-let analyser = null
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
 
-// audio1.play()
-audioSource = audioCtx.createMediaElementSource(audio1)
-analyser = audioCtx.createAnalyser()
-audioSource.connect(analyser)
-analyser.connect(audioCtx.destination)
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+  let audioSource = null
+  let analyser = null
 
-analyser.fftSize = 128
-const bufferLength = analyser.frequencyBinCount
-const dataArray = new Uint8Array(bufferLength)
-const barWidth = canvas.width / bufferLength
+  audio1.play()
+  audioSource = audioCtx.createMediaElementSource(audio1)
+  analyser = audioCtx.createAnalyser()
+  audioSource.connect(analyser)
+  analyser.connect(audioCtx.destination)
 
-let x = 0
-function animate() {
-  x = 0
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  analyser.getByteFrequencyData(dataArray)
-  let barHeight
-  for (let i = 0; i < bufferLength; i++) {
-    barHeight = dataArray[i]
-    const red = (i * barHeight) / 10
-    const green = i * 4
-    const blue = barHeight / 4 - 12
-    ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`
-    ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
-    x += barWidth
+  analyser.fftSize = 128
+  const bufferLength = analyser.frequencyBinCount
+  const dataArray = new Uint8Array(bufferLength)
+  const barWidth = canvas.width / bufferLength / 2
+
+  // let x = 0
+  // function animate() {
+  //   x = 0
+  //   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  //   analyser.getByteFrequencyData(dataArray)
+  //   drawVisualizer({
+  //     bufferLength,
+  //     dataArray,
+  //     barWidth,
+  //   })
+  //   requestAnimationFrame(animate)
+  // }
+
+  let x = 0
+  function animate() {
+    x = 0
+    analyser.getByteFrequencyData(dataArray)
+    worker.postMessage({ bufferLength, dataArray, barWidth, x }, {})
+    requestAnimationFrame(animate)
   }
 
-  requestAnimationFrame(animate)
-}
-
-animate()
+  animate()
+})
---
+++ worker.js
@@ -0,0 +1,51 @@
+let canvas = null
+
+const drawVisualizer = ({ bufferLength, dataArray }) => {
+  const barWidth = canvas.width / 2 / bufferLength // the width of each bar in the canvas
+  let firstX = 0 // used to draw the bars one after another. This will get increased by the width of one bar
+
+  const ctx = canvas.getContext('2d')
+  ctx.clearRect(0, 0, canvas.width, canvas.height) // clears the canvas
+
+  let barHeight
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i] * 4
+    const red = (i * barHeight) / 30
+    const green = i * 4
+    const blue = barHeight / 4 - 12
+    ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`
+    ctx.fillRect(
+      canvas.width / 2 - firstX, // this will start the bars at the center of the canvas and move from right to left
+      canvas.height - barHeight,
+      barWidth,
+      barHeight
+    )
+    firstX += barWidth // increases the x value by the width of the bar
+  }
+
+  let secondX = 0
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i] * 4
+    const red = (i * barHeight) / 30
+    const green = i * 4
+    const blue = barHeight / 4 - 12
+    ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`
+    ctx.fillRect(
+      canvas.width / 2 + secondX,
+      canvas.height - barHeight,
+      barWidth,
+      barHeight
+    ) // this will continue moving from left to right
+    secondX += barWidth // increases the x value by the width of the bar
+  }
+}
+
+onmessage = function (e) {
+  console.log('Worker: Message received from main script')
+  const { bufferLength, dataArray, canvas: canvasMessage } = e.data
+  if (canvasMessage) {
+    canvas = canvasMessage
+  } else {
+    drawVisualizer({ bufferLength, dataArray })
+  }
+}
